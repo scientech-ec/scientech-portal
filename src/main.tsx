f7b3c05abc578f02b4fa2a6b0f34020aee3c60f1
William Morales <william.rolando88@hotmail.com>
2022-09-12 21:26:22 -0500

    Reset `main`

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
-import { ThemeProvider } from '@mui/material/styles';
-import { scientechTheme } from './modules/customTheme';
 import App from './App';
 import './index.css';
-import store from './store/store';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <ThemeProvider theme={scientechTheme}>
-          <App />
-        </ThemeProvider>
-      </BrowserRouter>
-    </Provider>
+    <App />
   </React.StrictMode>
 );
