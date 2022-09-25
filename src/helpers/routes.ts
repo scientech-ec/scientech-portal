a50adc6470271fc934cceb3b1ba2b9f1cf19b8b7
William Morales <william.rolando88@hotmail.com>
2022-09-25 15:46:43 -0500

    Add dashboard routes

--- src/helpers/routes.ts
+++ src/helpers/routes.ts
@@ -1,29 +1,34 @@
-import { RoutesInterface } from '../interfaces/routesInterface';
+import { RoutesInterface } from "../interfaces/routesInterface";
 
 const routes: RoutesInterface = {
   client: {
-    home: { title: 'Inicio | Scientech Ecuador', name: 'Inicio', target: '/' },
+    home: { title: "Inicio | Scientech Ecuador", name: "Inicio", target: "/" },
     products: {
-      title: 'Catálogo | Scientech Ecuador',
-      name: 'Productos',
-      target: '/productos',
+      title: "Catálogo | Scientech Ecuador",
+      name: "Productos",
+      target: "/productos",
     },
     contact: {
-      title: 'Contacto | Scientech Ecuador',
-      name: 'Contacto',
-      target: '/contacto',
+      title: "Contacto | Scientech Ecuador",
+      name: "Contacto",
+      target: "/contacto",
     },
-    'under-construction': {
-      title: 'Esperanos | Scientech Ecuador',
-      name: 'Contacto',
-      target: '/under-construction',
+    "under-construction": {
+      title: "Esperanos | Scientech Ecuador",
+      name: "Contacto",
+      target: "/under-construction",
     },
   },
   employee: {
     login: {
-      title: 'Login | Scientech Ecuador',
-      name: 'Login',
-      target: '/login',
+      title: "Login | Scientech Ecuador",
+      name: "Login",
+      target: "/login",
+    },
+    dashboard: {
+      title: "Dashboard | Scientech Ecuador",
+      name: "Dashboard",
+      target: "/dashboard",
     },
   },
 };
