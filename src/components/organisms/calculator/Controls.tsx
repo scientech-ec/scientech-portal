b9f9f184c209f85b7177c5063dae312c5fb7080e
William Morales <william.rolando88@hotmail.com>
2022-10-16 17:04:45 -0500

    Add save function to button

--- src/components/organisms/calculator/Controls.tsx
+++ src/components/organisms/calculator/Controls.tsx
@@ -3,14 +3,14 @@ import { useCalculator } from "../../../hooks/useCalculator";
 import CustomButton from "../../atoms/apps/CustomButton";
 
 const Controls: React.FC = () => {
-  const { reset, compute } = useCalculator();
+  const { reset, compute, save } = useCalculator();
   return (
     <header className="flex w-full items-center justify-between rounded-md border p-2 shadow-sm">
       <h4 className="">Calculadora de Importaciones</h4>
       <div className="grid grid-cols-4 gap-2">
         <CustomButton onClick={compute}>Calcular</CustomButton>
         <CustomButton onClick={reset}>Nuevo</CustomButton>
-        <CustomButton>Guardar</CustomButton>
+        <CustomButton onClick={save}>Guardar</CustomButton>
         <CustomButton>Recuperar</CustomButton>
       </div>
     </header>
