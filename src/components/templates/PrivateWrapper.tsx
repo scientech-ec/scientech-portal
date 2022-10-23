a123ccad18d050eef25e4ea1a50c6772f9c95696
William Morales <william.rolando88@hotmail.com>
2022-10-23 08:54:18 -0500

    Refactor wrapper component

--- src/components/templates/PrivateWrapper.tsx
+++ src/components/templates/PrivateWrapper.tsx
@@ -1,12 +1,12 @@
 import React, { useEffect } from "react";
-import { Link, Outlet, useLocation, useNavigate } from "react-router-dom";
+import { Outlet, useLocation, useNavigate } from "react-router-dom";
 import { protectedRoutes } from "../../helpers/routes";
 import { useRealmApp } from "../../hooks/useRealmApp";
-import Scientech from "../atoms/logos/Scientech";
 import AppSelector from "../molecules/AppSelector";
+import DashboardHeader from "../molecules/DashboardHeader";
 
 const PrivateWrapper: React.FC = () => {
-  const { currentUser, logOut } = useRealmApp();
+  const { currentUser } = useRealmApp();
   const { pathname } = useLocation();
   const navigate = useNavigate();
 
@@ -26,21 +26,7 @@ const PrivateWrapper: React.FC = () => {
   }, [pathname]);
   return (
     <>
-      <header className="sticky flex items-center justify-between bg-scientech p-2">
-        <Scientech variant="white" className="h-10" />
-
-        <div className="flex gap-2 ">
-          <Link
-            className="rounded-md bg-black/20 py-2 px-4"
-            to={protectedRoutes.dashboard.target}
-          >
-            Casita
-          </Link>
-          <button className="rounded-md bg-black/20 py-2 px-4" onClick={logOut}>
-            Salir
-          </button>
-        </div>
-      </header>
+      <DashboardHeader />
       <main className="flex">
         <AppSelector />
         <Outlet />
