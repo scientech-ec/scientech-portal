73ea9237fe69042f3ff86b0d3516d64205dff977
William Morales <william.rolando88@hotmail.com>
2022-10-30 11:47:29 -0500

    Add modal preview

--- src/components/organisms/calculator/Controls.tsx
+++ src/components/organisms/calculator/Controls.tsx
@@ -5,7 +5,12 @@ import Modal from "../../atoms/Modal";
 
 const Controls: React.FC = () => {
   const { reset, compute, save } = useCalculator();
-  const [openSaveModal, setOpenSaveModal] = useState(false);
+  const [saveModal, setSaveModal] = useState(false);
+
+  const handleSave = () => {
+    save();
+    setSaveModal(false);
+  };
   return (
     <React.Fragment>
       <header className="flex w-full items-center justify-between rounded-md border p-2 shadow-sm">
@@ -13,19 +18,36 @@ const Controls: React.FC = () => {
         <div className="grid grid-cols-4 gap-2">
           <CustomButton onClick={compute}>Calcular</CustomButton>
           <CustomButton onClick={reset}>Nuevo</CustomButton>
-          <CustomButton onClick={() => setOpenSaveModal(!openSaveModal)}>
+          <CustomButton onClick={() => setSaveModal(true)}>
             Guardar
           </CustomButton>
           <CustomButton>Recuperar</CustomButton>
         </div>
       </header>
       <Modal
-        open={openSaveModal}
-        onClose={() => setOpenSaveModal(false)}
-        onConfirm={() => setOpenSaveModal(false)}
+        open={saveModal}
+        onClose={() => setSaveModal(false)}
+        onConfirm={handleSave}
         title="Guardar?"
       >
-        Here comes the content
+        <div>
+          <label htmlFor="documentName">Nombre:</label>
+          <input
+            type="text"
+            name="documentName"
+            id="documentName"
+            placeholder="Escribe un nombre"
+          />
+        </div>
+        <div>
+          <label htmlFor="documentDescription">Descripcion:</label>
+          <input
+            type="text"
+            name="documentDescription"
+            id="documentDescription"
+            placeholder="Escribe un nombre"
+          />
+        </div>
       </Modal>
     </React.Fragment>
   );
