3708d0aacd5d2e11ed83a83b45494a6f2f4bad5d
William Morales <william.rolando88@hotmail.com>
2022-11-04 21:25:20 -0500

    Add mongodb collection configuration

--- src/services/mongoDB/importCalculatorConfig.ts
+++ src/services/mongoDB/importCalculatorConfig.ts
@@ -1,5 +1,11 @@
-export const importCalculatorConfig = {
+export const importCalculatorData = {
   cluster: import.meta.env.VITE_CLUSTER_NAME,
-  db: import.meta.env.VITE_DB_NAME,
-  collection: import.meta.env.VITE_IMPORT_CALCULATOR_COLLECTION,
+  db: import.meta.env.VITE_DB_IMPORT_CALCULATOR,
+  collection: import.meta.env.VITE_IMPORT_CALCULATOR_DATA,
+};
+
+export const importCalculatorHeader = {
+  cluster: import.meta.env.VITE_CLUSTER_NAME,
+  db: import.meta.env.VITE_DB_IMPORT_CALCULATOR,
+  collection: import.meta.env.VITE_IMPORT_CALCULATOR_HEADER,
 };
