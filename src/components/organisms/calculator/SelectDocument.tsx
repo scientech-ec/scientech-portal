0c6670330d11caf75eef5d91451363bf52b5adee
William Morales <william.rolando88@hotmail.com>
2022-11-15 19:30:52 -0500

    Style modals

--- src/components/organisms/calculator/SelectDocument.tsx
+++ src/components/organisms/calculator/SelectDocument.tsx
@@ -1,3 +1,4 @@
+import { CheckIcon, TrashIcon, XIcon } from "@heroicons/react/outline";
 import React, { useEffect, useState } from "react";
 import { toLocalDateAndTime } from "../../../helpers/date";
 import { useCalculator } from "../../../hooks/useCalculator";
@@ -36,30 +37,64 @@ const SelectDocument: React.FC<Props> = ({ handleClose }) => {
 
   return (
     <>
-      <div className="grid grid-cols-12">
-        <div>No.</div>
-        <div className="col-span-2">Nombre</div>
-        <div className="col-span-4">Descripción</div>
-        <div className="col-span-1">Artículos</div>
-        <div className="col-span-2">Fecha y Hora</div>
-        <div className="col-span-2">Acción</div>
+      <div className="grid grid-cols-12 gap-1 border-b px-4 py-6">
+        <div className="rounded-md border bg-sky-500 text-center ">No.</div>
+        <div className="col-span-2 rounded-md border bg-sky-500 text-center">
+          Nombre
+        </div>
+        <div className="col-span-4 rounded-md border bg-sky-500 text-center">
+          Descripción
+        </div>
+        <div className="col-span-1 rounded-md border bg-sky-500 text-center">
+          Artículos
+        </div>
+        <div className="col-span-2 rounded-md border bg-sky-500 text-center">
+          Fecha y Hora
+        </div>
+        <div className="col-span-2 rounded-md border bg-sky-500 text-center">
+          Acción
+        </div>
 
         {documents.map((doc, index) => (
           <React.Fragment key={doc._id}>
-            <div>{index + 1}</div>
-            <div className="col-span-2">{doc.name}</div>
-            <div className="col-span-4">{doc.description}</div>
-            <div className="col-span-1">{doc.articlesQty}</div>
-            <div className="col-span-2">
+            <div className="flex items-center justify-center whitespace-nowrap rounded-md border px-1">
+              {index + 1}
+            </div>
+            <div className="col-span-2 flex items-center whitespace-nowrap rounded-md border px-1">
+              {doc.name}
+            </div>
+            <div className="col-span-4 flex items-center whitespace-nowrap rounded-md border px-1">
+              {doc.description}
+            </div>
+            <div className="col-span-1 flex items-center whitespace-nowrap rounded-md border px-1">
+              {doc.articlesQty}
+            </div>
+            <div className="col-span-2 flex items-center whitespace-nowrap rounded-md border px-1">
               {toLocalDateAndTime(doc.timestamp ?? Date.now())}
             </div>
-            <button onClick={() => handleOpen(doc)}>load</button>
-            <button onClick={() => handleDelete(doc)}>del</button>
+            <CustomButton
+              onClick={() => handleOpen(doc)}
+              variant="success"
+              icon={<CheckIcon className="h-4 w-4" />}
+            >
+              Cargar
+            </CustomButton>
+            <CustomButton
+              onClick={() => handleDelete(doc)}
+              variant="danger"
+              icon={<TrashIcon className="h-4 w-4" />}
+            >
+              Borrar
+            </CustomButton>
           </React.Fragment>
         ))}
       </div>
-      <div>
-        <CustomButton variant="danger" onClick={handleClose}>
+      <div className="flex justify-end gap-2 px-4 py-3">
+        <CustomButton
+          variant="danger"
+          onClick={handleClose}
+          icon={<XIcon className="h-5 w-5" />}
+        >
           Cancelar
         </CustomButton>
       </div>
