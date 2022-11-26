2fb872c613a0c97d1a670c707bb5a15dce9adf5b
William Morales <william.rolando88@hotmail.com>
2022-11-26 10:00:23 -0500

    Fix timestamp format

--- src/helpers/date.tsx
+++ src/helpers/date.tsx
@@ -1,4 +1,11 @@
-export const toLocalDateAndTime = (dateData: number) => {
-  const date = new Date(dateData);
-  return `${date.toLocaleDateString()} - ${date.toLocaleTimeString()}`;
+export const toLocalDateAndTime = (dateNumber: number) => {
+  const date = new Date(dateNumber);
+  return date.toLocaleTimeString([], {
+    year: "numeric",
+    month: "numeric",
+    day: "numeric",
+    hour: "numeric",
+    minute: "numeric",
+    hour12: false,
+  });
 };
