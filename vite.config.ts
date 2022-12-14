d9468d6f39ec04f0d886b0c8cc030b6c97135f26
William Morales <william.rolando88@hotmail.com>
2022-12-14 08:39:46 -0500

    Delete vite-plugin-compression

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import viteCompression from "vite-plugin-compression";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     port: 3000,
   },
-  plugins: [react(), viteCompression()],
+  plugins: [react()],
 });
