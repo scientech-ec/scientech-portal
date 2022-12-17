1ec9b875c55ec9656d6987586001f1dbdabe7db8
William Morales <william.rolando88@hotmail.com>
2022-12-17 08:45:37 -0500

    Add object interface

--- src/services/mongoDB/importCalculatorConfig.ts
+++ src/services/mongoDB/importCalculatorConfig.ts
@@ -1,10 +1,16 @@
-export const importCalculatorData = {
+export interface mongoDBDatabaseID {
+  cluster: string;
+  db: string;
+  collection: string;
+}
+
+export const importCalculatorData: mongoDBDatabaseID = {
   cluster: import.meta.env.VITE_CLUSTER_NAME,
   db: import.meta.env.VITE_DB_IMPORT_CALCULATOR,
   collection: import.meta.env.VITE_IMPORT_CALCULATOR_DATA,
 };
 
-export const importCalculatorHeader = {
+export const importCalculatorHeader: mongoDBDatabaseID = {
   cluster: import.meta.env.VITE_CLUSTER_NAME,
   db: import.meta.env.VITE_DB_IMPORT_CALCULATOR,
   collection: import.meta.env.VITE_IMPORT_CALCULATOR_HEADER,
